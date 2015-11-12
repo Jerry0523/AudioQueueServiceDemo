a3eb019cd667ab455e58a59a573161fccaa582d8
Jerry <jaymee0523@gmail.com>
2015-11-12 16:29:58 +0800

    Initial Commit

---
+++ AudioQueueServiceDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AudioQueueServiceDemo
+//
+//  Created by Jerry Wong's Mac Mini on 15/11/12.
+//  Copyright © 2015年 Jerry Wong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AudioQueueServiceDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AudioQueueServiceDemo
+//
+//  Created by Jerry Wong's Mac Mini on 15/11/12.
+//  Copyright © 2015年 Jerry Wong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
